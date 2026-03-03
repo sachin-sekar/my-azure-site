8720770753a630c10c53f6c00b9409874605ef2c
sachin-sekar <sachinog55@gmail.com>
2026-03-03 17:45:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    const messageElement = document.getElementById('display-message');
+    messageElement.innerHTML = "🚀 Hello! Your website is live and interactive.";
+    messageElement.style.color = "#0078d4";
+    messageElement.style.marginTop = "20px";
+    messageElement.style.fontWeight = "bold";
+}
